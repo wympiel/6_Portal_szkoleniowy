5da73a3ea4fa6853fac61c4789d5c4c7671bd3ea
wympiel <wymp10@gmail.com>
2021-01-10 10:41:40 +0100

    third commit - registration form second version

--- sda_project/users/views.py
+++ sda_project/users/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Konto zostało założone dla użytkownika {username}!')
+            return redirect('register') # trzeba zmienić na stronę główną portalu!!!
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
